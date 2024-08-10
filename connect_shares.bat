33cd496ef70584945e6c40896c5aa06da8324243
darealdemayo <97548649+darealdemayo@users.noreply.github.com>
2024-08-10 05:37:39 +0200

    Update connect_shares.bat

--- connect_shares.bat
+++ connect_shares.bat
@@ -1,24 +1,27 @@
 @echo off
 setlocal
 
+:: Define the username and server
 set USERNAME=Mario
 set SERVER=nas.26.dengg.one
 
+:: Prompt for the password (input will not be visible)
 set /p PASSWORD=Please enter the password: 
 echo.
 
 :: Save the credentials using cmdkey
 cmdkey /add:%SERVER% /user:%USERNAME% /pass:%PASSWORD%
 
-net use D: \\%SERVER%\downloads /user:%USERNAME% /persistent:yes
-net use G: \\%SERVER%\Gerlinde /user:%USERNAME% /persistent:yes
-net use I: \\%SERVER%\images /user:%USERNAME% /persistent:yes
-net use M: \\%SERVER%\MarioPrivat /user:%USERNAME% /persistent:yes
-net use P: \\%SERVER%\projects /user:%USERNAME% /persistent:yes
-net use T: \\%SERVER%\techwiz-services /user:%USERNAME% /persistent:yes
-net use U: \\%SERVER%\techwiz-projects /user:%USERNAME% /persistent:yes
-net use Y: \\%SERVER%\media /user:%USERNAME% /persistent:yes
-net use Z: \\%SERVER%\fileZ /user:%USERNAME% /persistent:yes
+:: Connect to the network shares persistently with saved credentials
+net use D: \\%SERVER%\downloads /savecred /persistent:yes
+net use G: \\%SERVER%\Gerlinde /savecred /persistent:yes
+net use I: \\%SERVER%\images /savecred /persistent:yes
+net use M: \\%SERVER%\MarioPrivat /savecred /persistent:yes
+net use P: \\%SERVER%\projects /savecred /persistent:yes
+net use T: \\%SERVER%\techwiz-services /savecred /persistent:yes
+net use U: \\%SERVER%\techwiz-projects /savecred /persistent:yes
+net use Y: \\%SERVER%\media /savecred /persistent:yes
+net use Z: \\%SERVER%\fileZ /savecred /persistent:yes
 
 :: Check if the connections were successful
 if %errorlevel%==0 (
